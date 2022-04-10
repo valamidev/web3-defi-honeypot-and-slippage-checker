fc1cec26a1fa5274c442d75772dd0da206cdcacc
T <52665225+valamidev@users.noreply.github.com>
2022-04-10 17:33:52 +0200

    Init version up

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # web3-defi-honeypot-and-slippage-checker
-Cross-chain deployed Smart-contract to detect Honeypot and Slippage for DeFi tokens. 
+
+Cross-chain deployed Smart-contract to detect Honeypot and Slippage for DeFi tokens.
+
+### How it works:
+
+There is no magic only EVM(Ethereum Virtual Machine) features are involved.
+The contract simulate a **buy/approve/sell** execution in a single transaction and evaluate the results. Work with any UniSwap2 Router interface.
+
+### Which chain it is already available?
+
+```
+  Cronos chain => '0xb5BAA7d906b985C1A1eF0e2dAd19825EbAb5E9fc'
+  Fantom Chain => '0x4208B737e8f3075fD2dCB9cE3358689452f98dCf'
+  Polygon Chain => '0xc817b3a104B7d48e3B9C4fbfd624e5D5F03757e0'
+  Avax - wait for deploy fund
+  Arbitrum - wait for deploy fund
+  Harmony - wait for deploy fund
+  Aurora - wait for deploy fund
+  SBC - wait for deploy fund
+  Moonbeam - wait for deploy fund
+  Hoo Smart Chain - wait for deploy fund
+```
+
+### How to get this deployed to a new chain?
+
+To get this deployed we need some native/gas currency to deploy the smart contract
+
+```
+Fund address: 0x4030D79d04eE9c437A6B0470e7dcbB40aE796780
+```
+
+or you can deploy the contract ByteCode.
+
+### Why the contract is not open-source?
+
+The ByteCode and the ABI is. The full source will be, once it is deployed majority of the chains.
+
+### How to use?
+
+There is a TypeScript code snippet (example/index.ts)
+
+```
+const RunHoneyContract = async (
+  from: string, // Any existing address on the blockchain e.g. 0x573fbc5996bfb18b3f9b9f8e96b774905bcdc8b6 (find one from the Top Accounts https://cronoscan.com/accounts)
+  to: string, // The Honeypot checker contract Address e.g. 0xb5BAA7d906b985C1A1eF0e2dAd19825EbAb5E9fc
+  token: string, // the address of the token e.g. 0x062E66477Faf219F25D27dCED647BF57C3107d52 (wBTC)
+  router: string, // the DEX router address e.g.  0x145677fc4d9b8f19b5d56d1820c48e0443049a30 (MMfinance router on Cronos)
+  rcpAddress: string // Provide your EVM node e.g. https://evm-cronos.crypto.org
+)
+
+Result:
+{
+  buyTax: 0,
+  sellTax: 0.3, // Passed 0.3% Tax detected
+  buyGasCost: 0,
+  sellGasCost: 0,
+  isHoneypot: 0
+}
+```
+
+### My contract is failing on the Honeypot check why?
+
+1. Required to have a native currency trading pair available (wETH,wBNB,wCro...). Why? because it make no sense to support route like WrappedCoin -> USDT -> AnyToken.
+2. The available liquidity is lower than your simulation required
+3. The Contract is broken or not satisfy the Uniswap2 de facto requirement. (Whitelisting, Blacklisting, Trade Disable, MaxTx, MaxWallet...)
+
+### Is this safe?
+
+1. Slippage calculation it is tested over 20.000 different Token pair.
+2. Honeypot checking could be bypassed it is still blockchain, but the aim here is to decrease risk.
---
+++ example/index.ts
@@ -0,0 +1,176 @@
+import BigNumber from "bignumber.js";
+import Web3 from "web3";
+
+const contractAbi = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "targetTokenAddress",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "idexRouterAddres",
+        type: "address",
+      },
+    ],
+    name: "honeyCheck",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "uint256",
+            name: "buyResult",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "tokenBalance2",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "sellResult",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "buyCost",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "sellCost",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "expectedAmount",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct honeyCheckerV5.HoneyResponse",
+        name: "response",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "router",
+    outputs: [
+      {
+        internalType: "contract IDEXRouter",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+const TEST_AMOUNT = 10 ** 18 * 2; // Equal with 2 wETH, 2 wBNB, 2 wCRO...
+const GAS_LIMIT = "4500000"; // 4.5 million Gas should be enough
+
+const RunHoneyContract = async (
+  from: string,
+  to: string,
+  token: string,
+  router: string,
+  rcpAddress: string
+) => {
+  let buyTax = 0;
+  let sellTax = 0;
+  let buyGasCost = 0;
+  let sellGasCost = 0;
+  let isHoneypot = 0;
+
+  const web3 = new Web3(rcpAddress);
+  const gasPrice = await web3.eth.getGasPrice();
+
+  const honeyCheck = new web3.eth.Contract(contractAbi as any);
+
+  const data = honeyCheck.methods.honeyCheck(token, router).encodeABI();
+
+  let honeyTxResult: any;
+
+  try {
+    honeyTxResult = await web3.eth.call({
+      // this could be provider.addresses[0] if it exists
+      from,
+      // target address, this could be a smart contract address
+      to,
+      // optional if you want to specify the gas limit
+      gas: GAS_LIMIT,
+      gasPrice,
+      // optional if you are invoking say a payable function
+      value: TEST_AMOUNT,
+      // nonce
+      nonce: undefined,
+      // this encodes the ABI of the method and the arguements
+      data,
+    });
+  } catch (error) {
+    return {
+      buyTax: -1,
+      sellTax: -1,
+      isHoneypot: 1,
+    };
+  }
+
+  const decoded = web3.eth.abi.decodeParameter(
+    "tuple(uint256,uint256,uint256,uint256,uint256,uint256)",
+    honeyTxResult
+  );
+
+  buyGasCost = decoded[3];
+  sellGasCost = decoded[4];
+
+  const res = {
+    buyResult: decoded[0],
+    leftOver: decoded[1],
+    sellResult: decoded[2],
+    expectedAmount: decoded[5],
+  };
+
+  buyTax =
+    (1 -
+      new BigNumber(res.buyResult)
+        .dividedBy(new BigNumber(res.expectedAmount))
+        .toNumber()) *
+    100;
+  sellTax =
+    (1 -
+      new BigNumber(res.sellResult)
+        .dividedBy(new BigNumber(TEST_AMOUNT))
+        .toNumber()) *
+      100 -
+    buyTax;
+
+  return {
+    buyTax,
+    sellTax,
+    buyGasCost,
+    sellGasCost,
+    isHoneypot,
+  };
+};
+
+RunHoneyContract(
+  "0x573fbc5996bfb18b3f9b9f8e96b774905bcdc8b6",
+  "0xb5BAA7d906b985C1A1eF0e2dAd19825EbAb5E9fc",
+  "0x062E66477Faf219F25D27dCED647BF57C3107d52",
+  "0x145677fc4d9b8f19b5d56d1820c48e0443049a30",
+  "https://evm-cronos.crypto.org"
+)
+  .catch()
+  .then((e) => console.log(e));
